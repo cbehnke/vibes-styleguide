dad1059d7bf4f03f9bbcc41612aebeb25c044a42
Chad Behnke <chad.behnke@vibes.com>
2017-02-02 17:23:49 -0600

    Moving form CSS into its own file

--- config.rb
+++ config.rb
@@ -9,16 +9,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # General configuration
 redirect "index.html", to: "introduction.html"
-
 config[:trailing_slash] = false
 
 # Reload the browser automatically whenever files change
@@ -32,28 +24,15 @@ end
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  activate :directory_indexes
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-end
-
-
 helpers do
   def check_current_path(path)
     if path == current_page.path
       return "active"
     end
   end
+end
+
+# Build-specific configuration
+configure :build do
+  activate :directory_indexes
 end
