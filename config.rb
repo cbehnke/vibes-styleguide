39ebd321481c14b91447d4d778b370dc60849682
Chad Behnke <chad.behnke@vibes.com>
2017-02-02 11:12:06 -0600

    Creating styleguide page layout

--- config.rb
+++ config.rb
@@ -17,6 +17,9 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
+redirect "index.html", to: "introduction.html"
+
+config[:trailing_slash] = false
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -38,6 +41,7 @@ end
 
 # Build-specific configuration
 configure :build do
+  activate :directory_indexes
   # Minify CSS on build
   # activate :minify_css
 
